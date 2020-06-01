95e447e84c99181a6fb2cdc3d9383161448901db
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-06-01 19:02:12 +0500

    s

--- MultiplicationTable.py
+++ MultiplicationTable.py
@@ -6,4 +6,4 @@ num = int(input("Enter any number you want to find multiplication table of: "))
 
 # For loop will Iterate 10 times from i = 1 to 10.
 for i in range(1, 11): # in ranges first one is inclusive, second one = 11, is excluded.
-   print(num, 'x', i, '=', num*i)
+   print(num, 'x', i, '=', num*i) # printing the table now.
