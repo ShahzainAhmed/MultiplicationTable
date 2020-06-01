19109ba591b3694da4ecdbc5ae4bd08d1875a4ff
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-06-01 18:53:30 +0500

    s

--- MultiplicationTable.py
+++ MultiplicationTable.py
@@ -2,7 +2,7 @@
 
 # To take input from the user
 
-# num = int(input("Display multiplication table of? "))
+num = int(input("Display multiplication table of? "))
 
 # Iterate 10 times from i = 1 to 10
 for i in range(1, 11): # in ranges first one is inclusive, second one = 11, is excluded.
